3191280fa6eaa4052141e8ddc89a5b2321130f37
Janghyun Son <jhson989@gmail.com>
2021-12-22 16:29:28 +0900

    코드 확장을 위한 리팩토링

--- server/src/index.js
+++ server/src/index.js
@@ -2,49 +2,17 @@ const express = require('express');
 const { ApolloServer, gql} = require('apollo-server-express');
 require('dotenv').config();
 
+// Data schema
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+
 
 // Configuration
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-
-
-// Schema
-const typeDefs = gql`
-    type Note {
-        id: ID!
-        content: String
-        author: String
-    }
-    type Query {
-        notes: [Note]
-        note(id: ID): Note
-    }
-    type Mutation {
-        newNote(content: String!): Note
-    }
-`;
-
-const resolvers = {
-    Query: {
-        notes: async() => {
-            return await models.Note.find();
-        },
-        note: async (parent, args) => {
-            return models.Note.findById(args.id);
-        }
-    },
-    Mutation: {
-        newNote: async (parent, args) => {
-            return await models.Note.create({
-                content: args.content,
-                author: "abc"
-            })
-        }
-    }
-}
-
 
 // App running
 const app = express();
@@ -55,14 +23,18 @@ async function startServer() {
     server = new ApolloServer({
         typeDefs,
         resolvers,
+        context: () => {
+            return {models};
+        },
         introspection: true,
         playground: true,
     });
     await server.start();
     server.applyMiddleware({ app, path:'/api' });
 }
-
 startServer();
+
+
 app.listen( { port }, () => 
     console.log('GraphQL Server running on http://localhost:'+port+server.graphqlPath)
 );
