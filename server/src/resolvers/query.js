a7edc898d46aedc42890e40c873ddfe58b9d3968
Janghyun Son <jhson989@gmail.com>
2021-12-23 12:58:45 +0900

    사용자 계정 생성 및 인증 기능 추가

--- server/src/resolvers/query.js
+++ server/src/resolvers/query.js
@@ -1,8 +1,14 @@
 module.exports =  {
-    notes: async(parent, args, {models}) => {
-        return await models.Note.find();
+    notes: async(parent, args, {models, user}) => {
+        if (user == true)
+            return await models.Note.find();
+        else
+            throw new Error("??");
     },
-    note: async (parent, args, {models}) => {
-        return models.Note.findById(args.id);
+    note: async (parent, args, {models, user}) => {
+        if (user == true)
+            return models.Note.findById(args.id);
+        else
+            throw new Error("??");
     }
 }
